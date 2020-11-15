0d25ebcd7709e6a2ef90f1b738d0acfed5548d85
Michal-W-dev <70209633+Michal-W-dev@users.noreply.github.com>
2020-11-15 11:40:29 +0100

    Update index.js
    Add functionality to date and display of time

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
+const dom = {}
+
+function selectDomElements(string) {
+    const arr = string.replace(/\s/g, '').split(',')
+    arr.forEach((val) => (dom[val] = document.querySelector(`.${val}`)));
+}
+
+selectDomElements('secHand, minHand, hourHand, hrsMins, dayNum, dayName, month, year')
 const hand = document.querySelectorAll('.hand')
-const secondHand = document.querySelector('.second-hand')
-const minHand = document.querySelector('.min-hand')
-const hourHand = document.querySelector('.hour-hand')
+
 
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds()
     const secondsDeg = seconds * 6 + 90
-    secondHand.style.transform = `rotate(${secondsDeg}deg) scale(1 ,0.8)`;
+    dom.secHand.style.transform = `rotate(${secondsDeg}deg) scale(1 ,0.8)`;
     const mins = now.getMinutes()
     const minsDeg = mins * 6 + 90
-    minHand.style.transform = `rotate(${minsDeg}deg) scale(0.9, 1)`;
+    dom.minHand.style.transform = `rotate(${minsDeg}deg) scale(0.9, 1)`;
     const hrs = now.getHours()
     const hrsDeg = hrs * 360 / 12 + 90
-    hourHand.style.transform = `rotate(${hrsDeg}deg) scale(0.6, 1.1)`;
+    dom.hourHand.style.transform = `rotate(${hrsDeg}deg) scale(0.6, 1.1)`;
     if (secondsDeg === 90) { hand.forEach((val) => { val.classList.remove('transition') }); }
     else if (secondsDeg === 96) { hand.forEach((val) => { val.classList.add('transition') }); }
+    dom.hrsMins.innerText = `${('0' + hrs).slice(-2)}:${('0' + mins).slice(-2)}`
+
+    //DATE
+    dom.dayNum.innerText = now.getDate();
+    dom.dayName.innerText = now.toLocaleString('eng', { weekday: 'long' })
+    dom.month.innerText = now.toLocaleString('eng', { month: 'long' })
+    dom.year.innerText = now.getFullYear();
 }
 
 setInterval(setDate, 1000);
